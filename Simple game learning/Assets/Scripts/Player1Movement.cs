bd59b5216c65d988ab482d9fa25912d6b2b0054a
Max Shevchenko <frykt008@gmail.com>
2021-08-11 17:43:49 +0300

    first part of game

---
+++ Simple game learning/Assets/Scripts/Player1Movement.cs
@@ -0,0 +1,47 @@
+﻿
+using System;
+using JetBrains.Annotations;
+using UnityEngine;
+
+public class Player1Movement : MonoBehaviour
+{
+
+    public Rigidbody rb;
+    public float Forse = 2000f;
+    public float sideWayForce = 500f;
+    public float backWayForce = 500f;
+    public float jumpFor = 500f;
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        rb.AddForce(0, 0, Forse * Time.deltaTime);
+    if( Input.GetKey("d")){
+     rb.AddForce(sideWayForce * Time.deltaTime, 0, 0, ForceMode.VelocityChange);
+}
+        if (Input.GetKey("a"))
+        {
+            rb.AddForce(-sideWayForce * Time.deltaTime, 0, 0, ForceMode.VelocityChange);
+
+        }
+
+        if (Input.GetKey("s"))
+        {
+            rb.AddForce(0, 0, -backWayForce * Time.deltaTime);
+
+        }
+
+        if (Input.GetKey("space"))
+        {
+         rb.AddForce(0,jumpFor* Time.deltaTime, jumpFor* Time.deltaTime, ForceMode.VelocityChange);   
+        }
+
+
+        if(rb.position.y < -1f)
+    { FindObjectOfType<gamemanager>().EndGame();
+    }
+    }
+    
+
+}
+
---
+++ Simple game learning/Assets/Scripts/playertype2.cs
@@ -0,0 +1,18 @@
+﻿
+using System.Reflection.Emit;
+using System.Security.AccessControl;
+using UnityEngine;
+
+public class playertype2 : MonoBehaviour
+{
+    public Player1Movement movement;
+    
+    void OnCollisionEnter(Collision collisioninfo)
+    {
+        if (collisioninfo.collider.tag == "GameObject") 
+        {
+        movement.enabled = false; 
+        FindObjectOfType<gamemanager>().EndGame();}
+        
+    }
+}
