b317281827bea74ae5a2cf3e267f448d46cf84fe
Max Shevchenko <frykt008@gmail.com>
2021-09-19 15:19:15 +0300

    [~] modified script added jump

--- Simple game learning/Assets/Scripts/PlayerMovement.cs
+++ Simple game learning/Assets/Scripts/PlayerMovement.cs
@@ -1,6 +1,7 @@
 ﻿
 using System;
 using JetBrains.Annotations;
+using UnityEditorInternal;
 using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
@@ -10,11 +11,18 @@ public class PlayerMovement : MonoBehaviour
     public float movingSideForse = 2000f;
     public float sideLeftRIght = 500f;
     public float sideBack = 500f;
-    public float jump = 500f;
+    public bool ground = false;
 
-    
+    public void OnGround()
+   {
+        ground = true;
+        
+   }
 
-        // Update is called once per frame
+   
+       
+
+   // Update is called once per frame
     public void FixedUpdate()
     {
       // const float speed = 60;
@@ -39,8 +47,17 @@ public class PlayerMovement : MonoBehaviour
             }
             rb.AddForce(-sideLeftRIght * Time.deltaTime, 0, 0, ForceMode.VelocityChange);
         }
-        
-        
+    
+    if (Input.GetKey(KeyCode.Space) && ground != true)
+    {
+        Vector3 velocity = rb.velocity;
+        rb.velocity = new Vector3(0, 7, velocity.z);
+        OnGround();
+    }
+
+
+
+
         if(rb.position.y < -1f)
     { FindObjectOfType<GameManager>().EndGame();
     }
