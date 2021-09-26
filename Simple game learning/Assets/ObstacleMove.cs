89c54eeff096182012e3e8f29f96ddf7b0f62ed4
Max Shevchenko <frykt008@gmail.com>
2021-09-26 11:35:42 +0300

    [+] Created obstacle move script

---
+++ Simple game learning/Assets/ObstacleMove.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObstacleMove : MonoBehaviour
+{
+    [SerializeField] private float speed;
+    [SerializeField] private float maxLeft;
+    [SerializeField] private float maxRight;
+    [SerializeField] private bool moveRight = true;
+
+    void Update()
+    {
+        Move();
+        
+    }
+
+    private void Move()
+    {
+        float currentSpeed = speed;
+
+        if (!moveRight)
+
+        {
+            currentSpeed = currentSpeed * -1;
+        }
+
+        Vector3 position = new Vector3(currentSpeed * Time.deltaTime, 0, 0);
+        transform.position += position;
+    }
+}
