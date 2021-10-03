8a79f6b9f75ceb2a5372d474e8e2c0c58b5073cd
Max Shevchenko <frykt008@gmail.com>
2021-10-03 21:56:34 +0300

    [~] Add new script Moving obstacle list and draw points moving

---
+++ Simple game learning/Assets/Scripts/ObstacleMoveList.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObstacleMoveList : MonoBehaviour
+{
+    [SerializeField] private List<Vector3> points;
+    void Update()
+    {
+    }
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.yellow;
+
+        for (var i = 0; i < points.Count; i++)
+        {
+            var position = transform.position;
+            Vector3 currentPoint = points[i] + position;
+
+            Vector3 nextPoint;
+            if (i + 1 < points.Count)
+            {
+                nextPoint = points[i + 1] + position;
+            }
+            else
+            {
+                nextPoint = points[0] + position;
+            }
+            Gizmos.DrawSphere(currentPoint, 0.2f);
+            Gizmos.DrawLine(currentPoint, nextPoint);
+        }
+    }
+}
